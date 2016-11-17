db8a7d476013ba06f9a63050e0fa13f99d23e498
Daniel Thompson <danthwa@gmail.com>
2016-11-16 17:48:29 -0800

    included types

--- pokeget.js
+++ pokeget.js
@@ -7,15 +7,23 @@ function pokeGet(id){
     url: `http://pokeapi.co/api/v1/pokemon/${id}/`
   }).done(function(data){
     description = data.descriptions[0].resource_uri
-    console.log("Description:");
-    console.log(description);
-    $('#pokeBin').append(
-      `<div class="pokeCard">
-        <p>${data.name}</p>
-      </div>`
-    )
-    console.log("first description console log");
-    console.log(description);
+    console.log(data);
+    if(data.types[1]){
+      $('#pokeBin').append(
+        `<div class="pokeCard">
+          <h5>${data.name}</h5>
+          <p>${data.types[0].name}, ${data.types[1].name}</p>
+        </div>`
+      )
+    } else {
+      $('#pokeBin').append(
+        `<div class="pokeCard">
+          <h5>${data.name}</h5>
+          <p>${data.types[0].name}</p>
+        </div>`
+      )
+    }
+
     descriptionGet(description)
   })
 
@@ -25,8 +33,6 @@ function descriptionGet(description){
   $.ajax({
     url: `http://pokeapi.co${description}`
   }).done(function(data){
-    console.log(data);
-    console.log(data.description);
     $(".pokeCard").last().append(
       `<p>${data.description}</p>`
     )
